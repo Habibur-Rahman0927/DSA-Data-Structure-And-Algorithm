c9984f0cd4dee9b4044f3ce3e8b86a83e78d8acd
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2023-02-28 17:53:01 +0600

    solve some recursive array

--- DSA/07_recursion/02_factorial.py
+++ DSA/07_recursion/02_factorial.py
@@ -1,3 +1,5 @@
+import time
+start_time = time.time()
 def factorial(n):
     if n == 1:
         return 1
@@ -5,4 +7,6 @@ def factorial(n):
 
 
 print(factorial(4))
+
+print("Total Running time = {:.5f} seconds".format(time.time() - start_time))
 
