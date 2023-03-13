e9d94e4ea263101f210e1c1ae7f46323ea38e598
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2023-03-13 17:28:52 +0600

    solve some prolem and learn something new

--- recursive/10_recursive_reverse.py
+++ recursive/10_recursive_reverse.py
@@ -4,6 +4,7 @@ def reverse(strng):
     if len(strng) <= 1:
         return strng
     return strng[len(strng) - 1 ]+ reverse(strng[0: len(strng) - 1])
+    # return reverse(strng[1:]) + strng[0]
 
 print(reverse('python'))
 
